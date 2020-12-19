8640c5028f19a150629ab5e3211dc2e1f0e54d5a
Max Quordlepleen <53817552+MaxQuordlepleen@users.noreply.github.com>
2020-12-19 00:42:38 +0000

    Initial commit

---
+++ 2/1.js
@@ -0,0 +1,14 @@
+const input = require('./input');
+
+
+var i = 0;
+
+//var inputSet = input.realInput;
+var inputSet = input.testInput;
+
+var validpasswordcount = 0;
+
+for (i=0; i<inputSet.length; ++i) {
+  let pw = inputSet[i].split(':')[1].trim();
+  
+}
